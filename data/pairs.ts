b2297edbd5578eaf2ab1116754fdd495971b41e7
niktrix <nicky.gurbani@gmail.com>
2020-08-15 15:23:17 +0530

    Get token pair from file

---
+++ data/pairs.ts
@@ -0,0 +1,7 @@
+import { ChainId } from "@uniswap/sdk";
+
+export let tokens = {};
+tokens[ChainId.MAINNET] = {
+  DIA: "0x84cA8bc7997272c7CfB4D0Cd3D55cd942B3c9419",
+  WETH: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+};
--- trade.ts
+++ trade.ts
@@ -8,9 +8,12 @@ import {
   Trade,
   Fetcher,
   Fraction,
+  WETH,
 } from "@uniswap/sdk";
 import { TwilioWhastAppMessage } from "./services/message";
 import { config } from "./config";
+import {tokens} from "./data/pairs";
+
 
 
 let  delay = (ms: number) =>{
@@ -20,6 +23,10 @@ let  delay = (ms: number) =>{
 let alertRecievers = config.get("alertRecievers")
 let accountSid = config.get("twilio").accountSid
 let authToken = config.get("twilio").authToken
+let chain = ChainId.MAINNET
+let PAIR_TO_KEEP_EYE = ["DIA","WETH"]
+
+
 
 
 let main = async () => {
@@ -28,32 +35,30 @@ let main = async () => {
   let messageservice = new TwilioWhastAppMessage(accountSid,authToken)
 
   while (true) {
+
     try{
-    const DIA = await Fetcher.fetchTokenData(ChainId.MAINNET,"0x84cA8bc7997272c7CfB4D0Cd3D55cd942B3c9419")
-    const WETH = await Fetcher.fetchTokenData(ChainId.MAINNET,"0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
-    
-    let pair = await Fetcher.fetchPairData( WETH,DIA);
-    const DIA_TO_WETH_ROUTE = new Route([pair], DIA, WETH);
-    // let tradedia = new Trade(DIA_TO_WETH_ROUTE,new TokenAmount(WETH, '100000000000000000'), TradeType.EXACT_OUTPUT)
+    console.log("Fetching Token Data of ",PAIR_TO_KEEP_EYE[0])
+    const token0 = await Fetcher.fetchTokenData(chain,tokens[chain][PAIR_TO_KEEP_EYE[0]])
+    console.log("Fetching Token Data of ",PAIR_TO_KEEP_EYE[1])
+    const token1= await Fetcher.fetchTokenData(chain,tokens[chain][PAIR_TO_KEEP_EYE[1]])
+    let pair = await Fetcher.fetchPairData(token0,token1);
+    const TOKEN0_TOKEN1_ROUTE = new Route([pair], token0, token1);
+    // Sample Trade of 270 Tokens
+    let tradedia = Trade.exactIn(TOKEN0_TOKEN1_ROUTE,new TokenAmount(token0, '270000000000000000000'))
+    console.log("Trade",tradedia.executionPrice.invert().toSignificant(6))
 
-    let tradedia = Trade.exactIn(DIA_TO_WETH_ROUTE,new TokenAmount(DIA, '270000000000000000000'))
-    console.log("trade",tradedia.executionPrice.invert().toSignificant(6))
-
-    let p = pair.priceOf(WETH);
+    console.log("fetching Price ",PAIR_TO_KEEP_EYE[1])
+    let p = pair.priceOf(token1);
 
     if (p.greaterThan("130")) {
-      console.log("Buy Buy Buy");
       let message = "Dia Price is "+p.toSignificant(6)+"WETH :   Trade Price will be : "+tradedia.executionPrice.invert().toSignificant(6)
       messageservice.sendToMultiple(message,alertRecievers)
-      await delay(100000);
-
     }
     if (p.lessThan("160")) {
       let message = "Dia Price is "+p.toSignificant(6)+"WETH :   Trade Price will be : "+tradedia.executionPrice.invert().toSignificant(6)
       messageservice.sendToMultiple(message,alertRecievers)
     }
-    console.log("Price of DAI per Weth: ", p.toSignificant(6));
-    await delay(100000);
+
   }
   catch(err){
   console.log("Error wile running main", err)
