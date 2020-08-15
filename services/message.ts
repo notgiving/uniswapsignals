c4a4fe0ad44c9c75cba35684256e178f4c888daf
niktrix <nicky.gurbani@gmail.com>
2020-08-15 18:40:42 +0530

    add interval after sending message

--- services/message.ts
+++ services/message.ts
@@ -7,13 +7,15 @@ interface IMessage {
 
 export class TwilioWhastAppMessage implements IMessage {
   private client: twilio.Twilio;
+  private lastsent: Date;
+
   constructor(accountSid: string, authToken: string) {
     this.client = new twilio.Twilio(accountSid, authToken);
   }
 
   send(message: string, to: string): void {
     try {
-       this.client.messages
+      this.client.messages
         .create({
           body: message,
           from: "whatsapp:+14155238886",
@@ -21,27 +23,32 @@ export class TwilioWhastAppMessage implements IMessage {
         })
         .then((message) =>
           console.log("Twilio Message send, Message ID", message.sid)
-        )
+        );
     } catch (err) {
       console.log("Twilio Message send error", err);
     }
   }
 
   sendToMultiple(message: string, to: string[]): void {
-    to.forEach((phoneTo) => {
-      try {
-        this.client.messages
-          .create({
-            body: message,
-            from: "whatsapp:+14155238886",
-            to: "whatsapp:+91" + phoneTo,
-          })
-          .then((message) =>
-            console.log("Twilio Message send, Message ID", message.sid)
-          )
-      } catch (err) {
-        console.log("Twilio Message send error", err);
-      }
-    });
+    let nowdate = new Date();
+    // Add 5 minutes interval 
+    if (this.lastsent.getTime() - nowdate.getTime() > 5 * 60 * 1000) {
+      this.lastsent = new Date()
+      to.forEach((phoneTo) => {
+        try {
+          this.client.messages
+            .create({
+              body: message,
+              from: "whatsapp:+14155238886",
+              to: "whatsapp:+91" + phoneTo,
+            })
+            .then((message) =>
+              console.log("Twilio Message send, Message ID", message.sid)
+            );
+        } catch (err) {
+          console.log("Twilio Message send error", err);
+        }
+      });
+    }
   }
 }
